4b3dc455b303b3f06607a9874d84691fde59fd48
Sandriely F M Toledo GTADSI33A <109091362+Sandriely1999@users.noreply.github.com>
2024-09-08 23:41:23 -0300

    SellerDaoJDBC and DaoFactory

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,9 +13,9 @@ public class Main {
     public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
 
         Department obj = new Department(1,"Books");
-        System.out.println(obj);
-
         Seller seller = new Seller(21,"Bob", "bob@gmail.com" , new Date() , 3000.0, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(seller);
     }
 }
