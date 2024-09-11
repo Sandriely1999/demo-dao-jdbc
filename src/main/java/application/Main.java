22f36ba0d3129d1a4a5bba1e2620e4feb659a940
Sandriely F M Toledo GTADSI33A <109091362+Sandriely1999@users.noreply.github.com>
2024-09-11 17:58:52 -0300

    reusing instantiation

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -13,11 +13,9 @@ import model.entities.Seller;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-
-        //Department obj = new Department(1,"Books");
-        //Seller seller = new Seller(21,"Bob", "bob@gmail.com" , new Date() , 3000.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println("=== Test 1: seller findById ====");
 
         Seller seller = sellerDao.findById(3);
 
