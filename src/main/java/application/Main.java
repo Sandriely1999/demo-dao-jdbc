1c76da3cef4e299e64ebeb2dc01a63c020a88c9a
Sandriely F M Toledo GTADSI33A <109091362+Sandriely1999@users.noreply.github.com>
2024-09-11 17:43:26 -0300

    findById Implementation

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,21 +1,26 @@
 package application;
-
+import java.sql.*;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-import java.util.Date;
+
+
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
 
-        Department obj = new Department(1,"Books");
-        Seller seller = new Seller(21,"Bob", "bob@gmail.com" , new Date() , 3000.0, obj);
+        //Department obj = new Department(1,"Books");
+        //Seller seller = new Seller(21,"Bob", "bob@gmail.com" , new Date() , 3000.0, obj);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+
         System.out.println(seller);
     }
 }
